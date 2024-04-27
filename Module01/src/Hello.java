eb8c95a5ca4127af474653292925cdb78297ae45
lovedestroy <903020094@qq.com>
2024-04-27 17:40:02 +0800

    Initial commit

---
+++ Module01/src/Hello.java
@@ -0,0 +1,9 @@
+/**
+ * @author lovedestroy
+ * @date 2024/4/27 - 17:37
+ **/
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
---
+++ Module02/src/Main.java
@@ -0,0 +1,9 @@
+/**
+ * @author lovedestroy
+ * @date ${DATE} - ${TIME}
+ **/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
